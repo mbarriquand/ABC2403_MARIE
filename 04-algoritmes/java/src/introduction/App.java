2fb86582b55f9fb3d1666e2a2340d3e53d138c17
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-04-24 15:55:16 +0200

    types de variables

--- 04-algoritmes/java/src/introduction/App.java
+++ 04-algoritmes/java/src/introduction/App.java
@@ -13,16 +13,33 @@ public class App {
 		
 		String prenom; // Déclaration d'une variable "prénom" de type "chaine de caractères"
 		
+		
 		int age; // Déclaration d'une variable "age" de type "Nombre entier 32 bits" (valeur scalaire)
 		// (plage de valeurs d'un INT +/- -2 147 000 000 à + 2 147 000 000)
 		
+		
 		long soldeBancaire; // Déclaration d'une variable "soldeBancaire" de type "Nombre entier 64 bits" (valeur scalaire)
+		
 		
 		float tailleEnMetres; // Déclaration d'une variable "tailleEnMetre" de type "Nombre réel 32 bits" (valeur scalaire)
 		
+		
 		double tailleEnMetresPourLesTresGrands; // Déclaration d'une variable "tailleEnMetrePourLesTresGrands" de type "Nombre réel 64 bits" (valeur scalaire)
 		
+		
 		Boolean estMajeur; // Déclaration d'une variable "estMajeur" de type "Booléen" 0 ou 1, FALSE ou TRUE (valeur scalaire)
+		
+		
+		prenom = "Mare"; // Affectation d'une valeur à la variable "prenom"
+		
+		System.out.println(prenom);
+		
+		
+		prenom = "Marie"; // Modification de la valeur de la variable "prenom"
+		
+		System.out.println(prenom);
+		
+		System.out.println("Bonjour " + prenom + ", bienvenue ici !");
 		
 	}
 
