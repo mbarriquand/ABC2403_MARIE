b374be66f16c9ba08456d84e9ea0299490537d49
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-05-02 10:47:47 +0200

    préparatifs exercice tarif assurance

---
+++ 04-algoritmes/tarifAssurance/src/tarifAssurance/App.java
@@ -0,0 +1,16 @@
+package tarifAssurance;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		
+		
+		
+		sc.close();
+	}
+
+}
