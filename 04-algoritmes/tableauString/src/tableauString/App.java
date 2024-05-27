0417e1d35904e76530a524067f4a91d4e93a79aa
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-05-27 08:38:23 +0200

    correction tableau string

--- 04-algoritmes/tableauString/src/tableauString/App.java
+++ 04-algoritmes/tableauString/src/tableauString/App.java
@@ -3,9 +3,14 @@ package tableauString;
 public class App {
 
 	public static void main(String[] args) {
-		String [] tableau = new String[1];
+		String [] tableau = new String[6];
 		
-		tableau[0] = "a, e, i, o, u, y";	
+		tableau[0] = "a";
+		tableau[1] = "e";
+		tableau[2] = "i";
+		tableau[3] = "o";
+		tableau[4] = "u";
+		tableau[5] = "y";
 		
 		System.out.println(tableau[0]);
 
