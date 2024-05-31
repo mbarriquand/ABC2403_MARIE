22839dd5c8d59681b4f7a9450311da6ef5a3b3c0
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-05-31 10:00:34 +0200

    correction insertionTableau2

--- 04-algoritmes/insertionTableau2/src/insertionTableau2/App.java
+++ 04-algoritmes/insertionTableau2/src/insertionTableau2/App.java
@@ -1,15 +1,58 @@
 package insertionTableau2;
 
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Map;
+
 
 public class App {
 
 	public static void main(String[] args) {
-
-		Map<String, Double> tata;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String saisieUtilisateur;
+		
+		String[] legumesPrix = new String[1];
+		
+		String[] legumesPrix2;
+		
+		do {
+			System.out.println("Saisissez un fruit ou un légume : ");
+			
+			saisieUtilisateur = sc.nextLine();
+			
+			if (!saisieUtilisateur.equals("go")) {
+			
+			legumesPrix[legumesPrix.length -1] = saisieUtilisateur;
+				
+			legumesPrix2 = new String[legumesPrix.length+1];
+			
+			System.arraycopy(legumesPrix, 0, legumesPrix2, 0, legumesPrix2.length);
+			
+			legumesPrix = legumesPrix2;
+			
+			}
+			
+		}
+			while (!saisieUtilisateur.equals("go")); // comparaison de chaine de caractère, dans Java on passe par une fonction, equals. le ! est là pour demander l'inverse
+		
+		for (int i = 0; i < legumesPrix.length; i++) {
+			System.out.println(legumesPrix[i]);
+		}
+		
+		/* PARCOURIR UN TABLEAU A L'ENVERS
+		 * 
+		for (int i = legumesPrix.length-1; i >= 0; i--) {
+			
+		}
+		*/
+		
+		sc.close();
+		
+			
+		
+	/*	tentatives infructueuses
+	 * 
+	 * Map<String, Double> tata;
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -38,7 +81,7 @@ public class App {
 		
 		System.out.println("Le moins cher est : " + legume.get(0));
 					
-		sc.close();
+		sc.close(); */
 	}
 }
 
