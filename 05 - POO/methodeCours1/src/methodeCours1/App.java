67bf547936a9849063c4eb6c27c94fd4170070ca
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-06-04 16:00:32 +0200

    fin de journée

--- 05 - POO/methodeCours1/src/methodeCours1/App.java
+++ 05 - POO/methodeCours1/src/methodeCours1/App.java
@@ -6,20 +6,7 @@ import java.util.Date;
 
 public class App {
 
-	 public static void main(String[] args) {
-		 
-		 String prenom = "Jane";
-		 String nom = "Doe";
-		 String phrase = App.concatenerPrenomNom(prenom, nom);
-		 double resultat = App.additionner(17, 4.5);
-	
-		 
-		 System.out.println(resultat);
-		 System.out.println(phrase);
-		 App.afficherJourHeureDuMoment();
-	 }
-	 
-	 public static double additionner(double valeur1, double valeur2) {
+	public static double additionner(double valeur1, double valeur2) {
 		 double somme = 0;
 		 somme = valeur1 + valeur2;
 		 return somme;
@@ -34,10 +21,27 @@ public class App {
 	}
 	
 	public static void afficherJourHeureDuMoment() {
-		SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy - HH:mm:ss");
 	    Date date = new Date();
 	    System.out.println(formatDate.format(date));
 	}
 	
+	
+	public static void main(String[] args) {
+		 
+		 String prenom = "Jane";
+		 String nom = "Doe";
+		 String phrase = App.concatenerPrenomNom(prenom, nom);
+		 double resultat = App.additionner(17, 4.5);
+		 
+			 
+		 System.out.println(resultat);
+		 System.out.println(phrase);
+		 App.afficherJourHeureDuMoment();
+		 
+		
+		 
+	 }
+
 
 }
