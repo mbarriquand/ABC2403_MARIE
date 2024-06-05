fbc2e943063987ba733be2c176861dbeb14c7aa3
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-06-05 16:01:48 +0200

    le push du wtf

---
+++ 05 - POO/bouteilles/src/bouteille/App.java
@@ -0,0 +1,10 @@
+package bouteille;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Bouteille.remplir();
+	}
+
+}
---
+++ 05 - POO/bouteilles/src/bouteille/Bouteille.java
@@ -0,0 +1,19 @@
+package bouteille;
+
+public class Bouteille {
+	
+	public float contenanceMaxEnL;
+	public float contenuEnL;
+	public String typeDeContenu;
+	public Boolean estOuvert;
+	
+	
+	
+	
+	public static void remplir(); {
+		
+		estOuvert = true;
+	}
+	
+
+}
