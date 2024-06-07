d678d03895a6ef4bc03ff2198160c37c7bb81a19
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-06-07 09:50:54 +0200

    les bouteilles de l'angoisse

--- 05 - POO/bouteilles/src/bouteille/App.java
+++ 05 - POO/bouteilles/src/bouteille/App.java
@@ -4,7 +4,30 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		Bouteille.remplir();
+		/* Bouteille bouteille1 = new Bouteille();
+		
+		float a=1.2f;
+		
+		Bouteille bouteille2 = new Bouteille(a, 0.8f, true, "h2o");
+		
+		Bouteille bouteille3 = new Bouteille(bouteille2);
+		
+		// float contenanceEnCLDeBouteille2 = bouteille2.donneContenanceEnCL(); */
+		
+		Bouteille bouteilleCoca = new Bouteille(0.5f, 0.5f, false, "Coca");
+		
+		boolean openOK = bouteilleCoca.open();
+		
+		boolean openOK2 = bouteilleCoca.open();
+		
+		boolean closeOK = bouteilleCoca.close();
+		
+		boolean closeOK2 = bouteilleCoca.close();
+		
+		boolean fillOK = bouteilleCoca.fill();
+		
+		boolean fillOK2 = bouteilleCoca.fill();
+		
 	}
 
 }
--- 05 - POO/bouteilles/src/bouteille/Bouteille.java
+++ 05 - POO/bouteilles/src/bouteille/Bouteille.java
@@ -1,19 +1,105 @@
 package bouteille;
 
 public class Bouteille {
+
+	// ATTRIBUTES
+
+	private float contenanceMaxEnL;
+	private float contenuEnL;
+	private boolean estOuvert;
+	private String typeDeContenu;
+
+	// ACCESS AND MODIFIERS
+
+	// CONSTRUCTORS
+
+	/* default constructor where values are forced */
+	public Bouteille() {
+		contenanceMaxEnL = 1;
+		contenuEnL = 1;
+		estOuvert = false;
+		typeDeContenu = "H20";
+	} 
 	
-	public float contenanceMaxEnL;
-	public float contenuEnL;
-	public String typeDeContenu;
-	public Boolean estOuvert;
-	
-	
-	
-	
-	public static void remplir(); {
+	/* classic constructor */
+	// the user will provide the details
 		
-		estOuvert = true;
+	public Bouteille(float _contenanceMaxEnL,float _contenuEnL,boolean _estOuvert,String _typeDeContenu) {
+		
+		/* this refers to the object, not the class */
+		this.contenanceMaxEnL=_contenanceMaxEnL;
+		this.contenuEnL=_contenuEnL;
+		this.estOuvert=_estOuvert;
+		this.typeDeContenu=_typeDeContenu;
+		
 	}
 	
+	/* cloning constructor */
+	
+	public Bouteille(Bouteille _bouteilleARecopier) {
+		this.contenanceMaxEnL= _bouteilleARecopier.contenanceMaxEnL;
+		this.contenuEnL= _bouteilleARecopier.contenuEnL;
+		this.estOuvert= _bouteilleARecopier.estOuvert;
+		this.typeDeContenu= _bouteilleARecopier.typeDeContenu;
+		
+		// changing the content : 
+		// _bouteilleARecopier.typeDeContenu="Champagne";
+		
+	}
+	
+	// METHODS
+	
+	public boolean open() {
+		boolean managedToOpen;
+		if (this.estOuvert = true) {
+				managedToOpen = false;
+		}
+		else {
+				this.estOuvert = true;
+				managedToOpen = true;
+		}
+		return managedToOpen;
+	}
+	
+	public boolean close() {
+		boolean managedToClose;
+		if (this.estOuvert = false) {
+			managedToClose = true;
+		}
+		else {
+				this.estOuvert = false;
+				managedToClose = false;
+		}
+		return managedToClose;
+	}
+	
+	public boolean fill() {
+		boolean managedToFill;
+		if (this.contenuEnL != this.contenanceMaxEnL) {
+			managedToFill = true;
+			this.contenuEnL = this.contenanceMaxEnL;
+		}
+		else {
+			managedToFill = false;
+		}
+		return managedToFill;
+	}
+		
+	/*
+	
+	public boolean fill () {
+		boolean managedToFill;
+		managedToFill = this.contenanceMaxEnL;
+		return managedToFill;
+	}
+	
+	
+	public float donneContenanceEnCL() {
+		float result;
+		result = this.contenanceMaxEnL * 100;
+		return result;
+	} */
+	
+	
+	}
 
-}
