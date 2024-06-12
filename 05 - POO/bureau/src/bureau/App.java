fdb0b647bf3600e512d8868fc61567a8151abc63
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-06-12 15:51:38 +0200

    bureau

--- 05 - POO/bureau/src/bureau/App.java
+++ 05 - POO/bureau/src/bureau/App.java
@@ -3,7 +3,14 @@ package bureau;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Desk desk1 = new Desk(100f, 90f, 120f, 150f, true);
+		
+		boolean goesToHeight1 = desk1.goesToHeight1();
+		
+		Desk desk2 = new Desk(90f, 90f, 120f, 150f, true);
+		
+		boolean goesToHeight2 = desk2.goesToHeight1();
 
 	}
 
--- 05 - POO/bureau/src/bureau/Desk.java
+++ 05 - POO/bureau/src/bureau/Desk.java
@@ -2,9 +2,46 @@ package bureau;
 
 public class Desk {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// ATTRIBUTES
+	
+	private float height;
+	private float height1;
+	private float height2;
+	private float height3;
+	private boolean hasPower;
+	
+	// CONSTRUCTORS
+	
+	//default desk
+	public Desk() {
+		height1 = 90;
+		height2 = 120;
+		height3 = 150;
+		hasPower = true;
 	}
+
+	public Desk(float _height, float _height1,
+			float _height2, float _height3, boolean _hasPower) {
+		this.height = _height;
+		this.height1 = _height1;
+		this.height2 = _height2;
+		this.height3 = _height3;
+	}
+		
+	// METHODS
+		
+	public boolean goesToHeight1() {
+		boolean managesToGoToHeight1;
+		if (this.hasPower && this.height == this.height1) {
+			return managesToGoToHeight1 = false;
+		}
+		else if ( this.hasPower && this.height != this.height1) {
+			
+			managesToGoToHeight1 = true;
+		}
+		return managesToGoToHeight1;
+	}
+		
+	
 
 }
