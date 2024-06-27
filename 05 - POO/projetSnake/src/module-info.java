f66145719e80e11546b44246d2322c80cb0ddc8e
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-06-27 11:53:16 +0200

    début snake

---
+++ 05 - POO/projetSnake/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module projetSnake {
+	requires java.desktop;
+}
---
+++ 05 - POO/projetSnake/src/projetSnake/SnakeGame.java
@@ -0,0 +1,16 @@
+package projetSnake;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		
+		GameFrame frame = new GameFrame();
+		GamePanel panel = new GamePanel();
+		
+	}
+
+}
